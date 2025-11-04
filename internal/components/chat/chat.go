e7fefedb03334e5df8c42380c8fac9a0f9ef6e29
Jacobsky <jacobsky@users.noreply.github.com>
2025-11-04 23:12:56 +0900

    Refactored chat to use standard broker paradigm
    Signed-off-by: Jacobsky <jacobsky@users.noreply.github.com>

--- internal/components/chat/chat.go
+++ internal/components/chat/chat.go
@@ -2,8 +2,6 @@ package chat
 
 import (
 	components "checkpoint/internal/components/util"
-	"context"
-	"encoding/json"
 	"log/slog"
 	"net/http"
 
@@ -25,21 +23,51 @@ type Message struct {
 type handler struct {
 	// Note probably need some kind of mutex lock or channel for modifying things internally
 	message_store []Message
-	connections   map[context.Context]*datastar.ServerSentEventGenerator
 	tx            chan Message
+	rx            []chan Message
+	addRx         chan chan Message
+	delRx         chan (<-chan Message)
 }
 
 func newHandler() *handler {
-	tx := make(chan Message, channelBuffer)
 	h := &handler{
 		message_store: []Message{},
-		connections:   make(map[context.Context]*datastar.ServerSentEventGenerator),
-		tx:            tx,
+		tx:            make(chan Message, channelBuffer),
+		rx:            make([]chan Message, 0),
+		addRx:         make(chan chan Message, channelBuffer),
+		delRx:         make(chan (<-chan Message), channelBuffer),
 	}
-	go h.sendUpdates(tx)
+	go h.serve()
 	return h
 }
 
+func (h *handler) serve() {
+	slog.Info("Updater worker started")
+	for {
+		select {
+		case msg := <-h.tx:
+			slog.Info("Sending message", "message", msg, "active connections", len(h.rx))
+			for i, rx := range h.rx {
+				slog.Debug("broadcasting", "rxid", i)
+				rx <- msg
+			}
+		case channel := <-h.addRx:
+			slog.Debug("Opening channel", "channel", channel)
+			h.rx = append(h.rx, channel)
+		case channel := <-h.delRx:
+			slog.Debug("Closing channel", "channel", channel)
+			for i, ch := range h.rx {
+				if ch == channel {
+					h.rx[i] = h.rx[len(h.rx)-1]
+					h.rx = h.rx[:len(h.rx)-1]
+					close(ch)
+					break
+
+				}
+			}
+		}
+	}
+}
 func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
@@ -53,10 +81,11 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func (h *handler) pushMessage(message Message) {
 	if messageLength := len(h.message_store); messageLength > maxRetainedMessages {
-		h.message_store = append(h.message_store[1:messageLength-2], message)
+		h.message_store = append(h.message_store[1:], message)
 	} else {
 		h.message_store = append(h.message_store, message)
 	}
+	h.tx <- message
 }
 
 func RegisterRoutes(mux *http.ServeMux) {
@@ -73,7 +102,10 @@ func join(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// TODO: Find a way to use channels to better sequentially update the state
+// Note: Due to the way that the handlers work, when there is a disconnection, the entire state of the chat history is sent to the chat.
+// This site is intended to be an ephemeral chat with minimal history and only holds about 50 most recent messages.
+// It's a checkpoint, not a hangout spot.
+// There is a board for leaving a messages in internal/components/comments
 func (h *handler) chat(w http.ResponseWriter, r *http.Request) {
 	var store Message
 	err := datastar.ReadSignals(r, &store)
@@ -83,16 +115,28 @@ func (h *handler) chat(w http.ResponseWriter, r *http.Request) {
 
 	slog.Info("Chat Connected", "user", store.Nickname)
 	sse := datastar.NewSSE(w, r)
+
 	err = sse.PatchElementTempl(ChatBoxMessages(h.message_store))
-	h.connections[sse.Context()] = sse
 
 	if err != nil {
 		components.InternalError(sse, w, err)
 	}
+	listener := make(chan Message)
+	h.addRx <- listener
 	// Keep the context open until the connection closes (detectable via the request context)
-	<-sse.Context().Done()
-	slog.Info("Chat disconnected", "user", store.Nickname)
-	delete(h.connections, sse.Context())
+	for {
+		select {
+		case <-sse.Context().Done():
+			slog.Info("Chat disconnected", "user", store.Nickname)
+			h.delRx <- listener
+			return
+		case msg := <-listener:
+			err := sse.PatchElementTempl(ChatMessage(msg.Nickname, msg.Message), datastar.WithSelectorID(chatselector), datastar.WithModeAppend())
+			if err != nil {
+				slog.Error("Error occurred when patching", "error", err)
+			}
+		}
+	}
 }
 
 // Post a new message that will be polled
@@ -108,40 +152,11 @@ func (h *handler) postMessage(w http.ResponseWriter, r *http.Request) {
 
 	message := Message{store.Nickname, store.Message}
 	h.pushMessage(message)
-	h.tx <- message
 	// Do something to indicate that there is a new message
 	store.Message = ""
-	signals, err := json.Marshal(store)
-	if err != nil {
-		slog.Error("Json marshall error", "error", err)
-	}
 
-	err = sse.PatchSignals(signals)
+	err = sse.MarshalAndPatchSignals(&store)
 	if err != nil {
 		slog.Error("Patch Element Error", "message", err)
-	}
-}
-
-func (h *handler) sendUpdates(messages chan Message) {
-	slog.Info("Updater worker started")
-	for {
-		msg := <-messages
-		slog.Info("Sending message", "message", msg, "active connections", len(h.connections))
-		for i, rx := range h.connections {
-			slog.Debug("Connection Update", "count", i)
-			// skip if nil or closed
-			if rx == nil {
-				continue
-			}
-			if rx.IsClosed() {
-				slog.Info("Rx is closed skipping")
-				// TODO: prune the connection (or perhaps make a separate goroutine for pruning closed connections)
-				continue
-			}
-			err := rx.PatchElementTempl(ChatMessage(msg.Nickname, msg.Message), datastar.WithSelectorID(chatselector), datastar.WithModeAppend())
-			if err != nil {
-				slog.Error("Error occurred when patching", "error", err)
-			}
-		}
 	}
 }
