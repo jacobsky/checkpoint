7daae65046606662a83bd833cf039b5cca51f699
Jacobsky <jacobsky@users.noreply.github.com>
2025-11-18 13:52:35 +0900

    Intro sequence and reactivity better completed
    Added simple CSS transitions, better organized fragments, etc.
    
    Signed-off-by: Jacobsky <jacobsky@users.noreply.github.com>

--- README.md
+++ README.md
@@ -4,13 +4,12 @@ My own dedication to the internet checkpoints of the internet. Come join and rel
 
 - [x] Initial shared view prototype with datastar
     - [ ] Stream some music and show a static gif of a campfire
-- [ ] Single playing embedded youtube video set to the same shared timestamp for everyone (curated playlist of internet checkpoints with links to the site)
 - [x] Realtime ephemeral chat
-- [ ] Comments section
+- [x] Comments section
     - [x] Base DB Schema
-    - [ ] List Comments
-    - [ ] Post Comment
-    - [ ] Pinned comments (mostly for admin)
+    - [x] List Comments
+    - [x] Post Comment
+    - [x] Pinned comments (mostly for admin)
 - [ ] Error modal that can be used for an ephemeral "Something went wrong" message when a user does something silly or there is a backend error (patched via signals)
 
 ## Getting Started
--- internal/components/base.go
+++ internal/components/base.go
@@ -4,6 +4,7 @@ import (
 	"checkpoint/internal/components/util"
 	"log/slog"
 	"net/http"
+	"time"
 
 	"github.com/a-h/templ"
 	"github.com/starfederation/datastar-go/datastar"
@@ -13,13 +14,71 @@ func RegisterRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("GET /showchat", showchat)
 	mux.HandleFunc("GET /showmessages", showmessages)
 	mux.HandleFunc("GET /join", join)
-	mux.Handle("/", templ.Handler(Landing()))
+	mux.Handle("/", templ.Handler(Loading()))
+	mux.HandleFunc("GET /intro", intro)
+}
+
+var introduction_text = []string{
+	"Greetings weary traveler.",
+	"What brought you here?",
+	"Well, that's not important.",
+	"Regardless, please take a rest.",
+	"From where ever you hail.",
+	"Whatever your story",
+	"Welcome to the checkpoint",
+	"I hope you enjoy your stay",
+}
+
+func intro(w http.ResponseWriter, r *http.Request) {
+	// Play introduction
+	// 1. Show an empty page
+	// 2. Fade in a description of the checkpoint
+	// 3. Fade in the header
+	// 4, Fade in the footer
+	sse := datastar.NewSSE(w, r)
+
+	err := sse.PatchElementTempl(IntroContainer())
+	if err != nil {
+		util.InternalError(sse, w, err)
+	}
+
+	for _, text := range introduction_text {
+		err := sse.PatchElementTempl(Intro(text), datastar.WithSelectorID("introcontainer"), datastar.WithModeAppend())
+		if err != nil {
+			util.InternalError(sse, w, err)
+		}
+		time.Sleep(time.Second * 3)
+	}
+	// TODO: Implement a skip intro
+	// Skipping should likely be a second endpoint getting called from the frontend
+
+	err = sse.ExecuteScript(`document.getElementById("mainheader").style.opacity = 1;`)
+	if err != nil {
+		util.InternalError(sse, w, err)
+	}
+
+	err = sse.ExecuteScript(`document.getElementById("introcontainer").style.opacity=0;`)
+	if err != nil {
+		util.InternalError(sse, w, err)
+	}
+
+	time.Sleep(time.Second * 2)
+	err = sse.PatchElementTempl(LandingFrag())
+	if err != nil {
+		util.InternalError(sse, w, err)
+	}
+
+	time.Sleep(time.Second * 2)
+	err = sse.ExecuteScript(`document.getElementById("mainfooter").style.opacity = 1;`)
+	if err != nil {
+		util.InternalError(sse, w, err)
+	}
 }
 
 func join(w http.ResponseWriter, r *http.Request) {
 	slog.Info("User joined")
 	sse := datastar.NewSSE(w, r)
-	err := sse.PatchElementTempl(MainContent())
+	err := sse.PatchElementTempl(MainContentFrag(), datastar.WithSelectorID("main"), datastar.WithModeInner())
 	if err != nil {
 		util.InternalError(sse, w, err)
 	}
@@ -27,6 +86,7 @@ func join(w http.ResponseWriter, r *http.Request) {
 
 func showchat(w http.ResponseWriter, r *http.Request) {
 	sse := datastar.NewSSE(w, r)
+	// Transitino into the chat content
 	err := sse.PatchElementTempl(ChatContent())
 	if err != nil {
 		util.InternalError(sse, w, err)
@@ -35,6 +95,7 @@ func showchat(w http.ResponseWriter, r *http.Request) {
 
 func showmessages(w http.ResponseWriter, r *http.Request) {
 	sse := datastar.NewSSE(w, r)
+	// Transitino into the chat content
 	err := sse.PatchElementTempl(MessageBoardContent())
 	if err != nil {
 		util.InternalError(sse, w, err)
